a98659e2187964ae01119b780c8a6f0bbe583a7d
Adema Amangeldi <ademaamangeldi111@gmail.com>
2026-02-22 12:08:03 +0500

    feat: add Task, Filters and sorting types

---
+++ src/types/task.ts
@@ -0,0 +1,24 @@
+export type Priority = 'low' | 'medium' | 'high'
+
+export type FilterStatus = 'all' | 'active' | 'complleted'
+
+export type SortField = "priority" | 'title' | "createdAt"
+
+export type SortBy = 'asc' | 'desc'
+
+export interface Task {
+    id: string,
+    title: string,
+    description?: string,
+    priority: Priority,
+    completed: boolean,
+    createdAt: Date,
+}
+
+export interface Filters{
+    status: FilterStatus,
+    priority: Priority | 'all',
+    search: string,
+    sortField: SortField,
+    sortBy: SortBy
+}
